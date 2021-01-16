1f5bb4b4b8c990a3f50c153da6c68d90e1bead39
thomas <thomas.chalanson@sfr.fr>
2021-01-16 20:50:46 +0100

    install dependencies

--- index.js
+++ index.js
@@ -2,18 +2,10 @@ const express = require('express');
 
 const app = express();
 const port = 8080;
-const commentsRouter = require('./router/comments.controller');
-const monCompteRouter = require('./router/monCompte.controller');
-const profilRouter = require('./router/profil.controller');
-const profilAdminRouter = require('./router/profilAdmin.controller');
-const publicationsRouter = require('./router/publications.controller');
+const musicRouter = require('./router/music.controller');
 
 app.use(express.json());
-app.use(commentsRouter);
-app.use(monCompteRouter);
-app.use(profilRouter);
-app.use(profilAdminRouter);
-app.use(publicationsRouter);
+app.use(musicRouter);
 
 app.listen(port, (err) => {
   if (err) {
---
+++ router/music.controller.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const connection = require("../config");
+
+const router = express.Router();
+
+module.exports = router;
